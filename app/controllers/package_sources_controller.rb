35da3293a23da13f673eb88280ee3f7ea86f8e7d
Robert Hall <robert.hall@itatc.com>
2010-01-01 00:19:03 -0500

    Added field checkbox list

--- app/controllers/package_sources_controller.rb
+++ app/controllers/package_sources_controller.rb
@@ -1,4 +1,5 @@
 class PackageSourcesController < ApplicationController
+@@new_db_connection = nil
  active_scaffold :PackageSources do |config|
 	 config.columns=[:name, :package, :db_connection]
 	 #:package, :connection, :version]
@@ -6,7 +7,7 @@ class PackageSourcesController < ApplicationController
 	 config.columns[:db_connection].form_ui = :select
  end
  
-def get_tables_from_connection()
+def get_connection()
 	db_connection_details = DbConnection.find(:first, :conditions=> {:id => params[:id]})
 
 	begin
@@ -16,7 +17,10 @@ def get_tables_from_connection()
 	rescue ex
 		raise "Ex: " + ex
 	end
+end
 
+def get_tables_from_connection()
+	new_db_connection = get_connection()
 	if new_db_connection.connection.active? then
 		@source_tables = new_db_connection.connection.tables
 		respond_to do |format|
@@ -31,7 +35,16 @@ def get_tables_from_connection()
 		"\nNewDBConnection: " + new_db_connection.connected?.to_s
 	end
 end
+
+def get_fields_from_table
+	new_db_connection = get_connection()
+	@source_fields = new_db_connection.connection.columns(params["tbl"])
+	respond_to do |format|
+		format.html {render :partial=>"show_table_fields"}
+		format.xml {render :xml => @source_fields}
+	end
 end
 
+end
 class NewSourceConnection < ActiveRecord::Base
 end
